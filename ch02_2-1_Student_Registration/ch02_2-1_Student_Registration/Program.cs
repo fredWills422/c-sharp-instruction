01ce022923de9d29da9a7edb579f29bb6a5bf843
Fred Wills <willsf422@gmail.com>
2019-11-12 14:47:08 -0500

    student registration updated

---
+++ ch02_2-1_Student_Registration/ch02_2-1_Student_Registration/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ch02_2_1_Student_Registration 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Student Registration Form\n");
+            Console.Write("Enter first name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Enter last name: ");
+            string lastName = Console.ReadLine();
+            int yearOfBirth = GetInt("Enter year of birth: ");
+            Console.Write("\n");
+
+            Console.WriteLine("Welcome " + firstName + " " + lastName + "!");
+            Console.WriteLine("Your registration is complete.");
+            Console.WriteLine("Your temporary password is " + firstName + "*" + yearOfBirth);
+
+        }
+
+        private static int GetInt(string prompt) 
+        {
+            int i = 0;
+            bool success = false;
+
+            while (!success) 
+            {
+                Console.Write(prompt);
+                string s = Console.ReadLine();
+
+                if (int.TryParse(s, out i)) 
+                {
+                    success = true;
+                } 
+                else 
+                {
+                    Console.WriteLine("Error - not a whole number: " + s);
+                }
+
+            }
+
+            return i;
+        }
+    }
+}
